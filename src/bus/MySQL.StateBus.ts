332b6a9fdd14224cc8b080040f8e87fd7abced0c
Nicolas Pelletier <npelletier@apiculteur.io>
2020-03-03 18:46:51 +0100

    fix(MySQL.StateBus): refactor props handler

--- src/bus/MySQL.StateBus.ts
+++ src/bus/MySQL.StateBus.ts
@@ -1,11 +1,16 @@
 import * as Component from '../Component';
 import * as StateBus  from '../StateBus';
 import { State }      from '../State';
+import { merge }      from 'cqes-util';
 import * as MySQL     from 'cqes-mysql';
 
 export interface props extends Component.props {
-  MySQL:  MySQL.props;
-  owner?: string;
+  host?:     string;
+  user?:     string;
+  password?: string;
+  database?: string;
+  MySQL:     MySQL.props;
+  owner?:    string;
 }
 
 export class Transport extends Component.Component implements StateBus.Transport {
@@ -15,12 +20,17 @@ export class Transport extends Component.Component implements StateBus.Transport
   constructor(props: props) {
     super(props);
     if (props.MySQL == null)          props.MySQL = <any>{};
-    if (props.MySQL.name == null)     props.MySQL.name = props.name;
-    if (props.MySQL.user == null)     props.MySQL.user = 'cqes';
-    if (props.MySQL.password == null) props.MySQL.password = 'changeit';
-    if (props.MySQL.database == null) props.MySQL.database = 'cqes-' + props.name.toLowerCase();
-    this.mysql = new MySQL.MySQL(props.MySQL);
+    if (props.MySQL.url != null)      props.MySQL = merge(props.MySQL, MySQL.parseURL(props.MySQL.url));
+    if (props.MySQL.host == null)     props.MySQL.host = props.host || '127.0.0.1';
+    if (props.MySQL.user == null)     props.MySQL.user = props.user || this.process.vars.get('profile');
+    if (props.MySQL.password == null) props.MySQL.password = props.password || 'changeit';
+    if (props.MySQL.database == null) props.MySQL.database = props.database || 'cqes-' + props.name.toLowerCase();
+    this.mysql = new MySQL.MySQL(this.mkprops(props.MySQL));
     this.owner = props.owner || this.context + '.' + this.name;
+  }
+
+  public async start() {
+    await this.mysql.start();
   }
 
   public async save(state: State) {
@@ -48,6 +58,10 @@ export class Transport extends Component.Component implements StateBus.Transport
   public destroy(id: string): Promise<void> {
     const query = 'DELETE FROM `@states` WHERE `owner` = ? AND `stateId` = ?';
     return <any> this.mysql.request(query, [this.owner, id]);
+  }
+
+  public async stop(): Promise<void> {
+    await this.mysql.stop();
   }
 
 }

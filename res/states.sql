c1a6f7f937ae01fd883b130d9874db94dbb13850
Nicolas Pelletier <npelletier@apiculteur.io>
2020-12-23 16:48:48 +0100

    fix(MySQL): table update

--- res/states.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `@states` (
-  `stateId` varchar(128) COLLATE 'ascii_general_ci' NOT NULL,
-  `revision` int unsigned NOT NULL,
-  `data` longtext COLLATE 'utf8_general_ci' NOT NULL
-);
-ALTER TABLE `@states`
-ADD PRIMARY KEY `stateId` (`stateId`);
